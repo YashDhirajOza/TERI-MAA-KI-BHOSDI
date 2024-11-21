421529a6210ee2c7f110f9782c4a1814202266a7
Damm Bro <144452060+YashDhirajOza@users.noreply.github.com>
2024-11-21 21:08:28 +0530

    Add files via upload

---
+++ src/modules/home/components/FinancialServices/index.tsx
@@ -0,0 +1,20 @@
+import Accordion from '@modules/common/components/Accordion';
+import DocumentsList from '@modules/common/components/DocumentsList';
+import SplitBlocks from '@modules/common/components/SplitBlocks';
+import {lendingDocumentsList} from '@modules/home/components/About/data';
+
+const HomeFinancialServices = () => {
+	return (
+		<SplitBlocks
+			title="Фінансові послуги"
+			titleType="heading"
+			anchor="fin-services"
+		>
+			<Accordion title="Кредитування">
+				<DocumentsList list={lendingDocumentsList}/>
+			</Accordion>
+		</SplitBlocks>
+	);
+};
+
+export default HomeFinancialServices;
---
+++ src/utils/data.ts
@@ -0,0 +1,14 @@
+import { type INavigationList } from '@utils/types';
+
+export const navigationList: INavigationList[] = [
+	{ title: '', path: '#about', isActive: true },
+	{ title: '', path: '#fin-services', isActive: true },
+	{ title: '', path: '#reports', isActive: true },
+	{ title: '', path: '#protection', isActive: true },
+	{
+		title: '',
+		path: '#report-violation',
+		isActive: true,
+	},
+	{ title: '', path: '#contacts', isActive: true },
+];
